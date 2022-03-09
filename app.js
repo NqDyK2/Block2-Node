6d4cf3eec9cb9fd53e0b88dd05751319554d7423
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-03-09 20:13:53 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+//middleware
+const check = (req, res, next) => {
+    const status = true;
+    if(status){
+        console.log("Cho phép truy cập");
+        next();
+    }else{
+        console.log("Nothing");
+    }
+}
+app.get('/api/products', check, (req, res)=>{
+    const products = [
+        {id: 1, name: "Products A"},
+        {id: 2, name: "Products B"}
+    ]
+    res.json(products);
+})
+
+
+// const server = http.createServer((req, res) => {
+//     console.log("url", req.url);
+//     if(req.url === "/"){
+//         res.setHeader('Content-Type', "text/html");
+//         res.write('<html><body><h1>Home Page</h1></body></html>')
+//         res.end();
+//     }else if(req.url === "/api/products"){
+//         const products = [
+//             {id: 1, name: "Product A"},
+//             {id: 2 , name: "Product B"}
+//         ]
+//         res.end(JSON.stringify(products));
+//     }else{
+//         console.log("....");
+//     }
+// });
+
+const PORT = 3001; 
+app.listen(PORT, () => {
+    console.log("Server running in port:", PORT);
+})
