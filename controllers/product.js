a23466821c1cc3dc868a04b0ee7fa095bc05cb5d
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-03-14 09:30:51 +0700

    feat: update Day3 - Connect mongoDB with mongoose.

---
+++ controllers/product.js
@@ -0,0 +1,43 @@
+// fake data
+// const products = [
+//     {id: 1, name: "Product A"},
+//     {id: 2, name: "Product B"},
+//     {id: 3, name: "Product C"},
+//     {id: 4, name: "Product D"},
+//     {id: 5, name: "Product E"}
+// ];
+import mongoose from "mongoose";
+const Product = mongoose.model('Product', { name: String });
+
+// API thêm sản phẩm
+export const create = async (req, res) => {
+    try {
+        const product = await new Product(req.body).save();
+        res.json(product)    
+    } catch (error) {
+        res.status(400).json({
+            message: "Không thêm được sản phẩm anh ei"
+        })
+    }
+}
+// API list sản phẩm
+export const list = async (req, res) => { 
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi không tìm được sản phẩm"
+        })
+    }
+}
+export const read = (req, res) => {
+    res.json( products.find(item => item.id === +req.params.id));
+}
+
+export const remove = (req, res) => {
+    res.json(products.filter(item => item.id !== +req.params.id));
+}
+export const update = (req, res) => {
+    res.json(products.map(item => item.id == req.params.id ? req.body : item));
+}   
--- routes/product.js
+++ routes/product.js
@@ -1,35 +1,14 @@
 import { Router } from "express";
+import {list, read, create, remove, update } from "../controllers/product";
 import {checkAuth} from "../middlewares/checkAuth"
+
 const router = Router();
 
-// const check = (req, res, next ) => {
-//     const status = true;
-//     if(status){
-//         next();
-//     }else{
-//         console.log("Nothing");
-//     }
-// }
 
 
-
-// fake data
-const products = [
-    {id: 1, name: "Product A"},
-    {id: 2, name: "Product B"}
-];
-
-router.get('/products', checkAuth, (req, res) => {
-    res.json(products);
-});
-router.get('/product/:id', checkAuth, (req, res) => {
-    res.json(products.find(item => item.id === +req.params.id));
-});
-router.post('/products', checkAuth, (req, res) => {
-    products.push(req.body);
-    res.json(products);
-});
-router.delete('/product/:id', checkAuth, (req, res) => {
-    res.json(products.filter(item => item.id !== +req.params.id));
-});
+router.get('/products', checkAuth, list);
+router.get('/product/:id', checkAuth, read);
+router.post('/products', checkAuth, create);
+router.delete('/product/:id', checkAuth, remove);
+router.patch("/product/:id", checkAuth, update )
 export default router;
